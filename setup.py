7789b9c8a3b0b9df3eb764efd758a8c76c0019b0
seantyh <seantyh@gmail.com>
2022-03-05 14:58:13 +0800

    update version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CwnGraph',
-    version='0.1.5',
+    version='0.2.0',
     packages=find_packages(),
     license='GPL GNUv3',
     author="NTUGIL LOPE Lab",   
